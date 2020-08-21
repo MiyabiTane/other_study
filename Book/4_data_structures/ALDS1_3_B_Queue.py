106add195411b5862ce127dfc3249e605953570a
MiyabiTane <miyabitane@yahoo.co.jp>
2020-08-21 22:24:04 +0900

    ラウンドロビンスケジューリング

--- Book/4_data_structures/ALDS1_3_B_Queue.py
+++ Book/4_data_structures/ALDS1_3_B_Queue.py
@@ -1,15 +1,52 @@
+"""
+class Queue:
+    def __init__(n):
+        self.head = 0
+        self.tail = 0
+        self.n = 0
+        self.queue = []
+
+    def isEmpty():
+        return head == tail
+    
+    def isFull():
+        return head == (tail+1) % self.n
+    
+    def enqueue(pr, tm):
+        if isFull():
+            print("error")
+            exit(1)
+        self.queue[self.tail] = (pr, tm)
+        tail = 0 if tail+1 = self.n else tail+1
+
+    def dequeue():
+        if isEmpty():
+            print("error")
+            exit(1)
+        pr, tm = self.queue[head]
+        head = 0 if head+1 = self.n else head+1
+        return pr, tm
+"""
+
 def schedule(lst, q):
+    #環状のqueue
+    head = 0; tail = len(lst)-1
     time = 0
     ans = []
-    while lst:
-        pr, tm = lst.pop(0)
+    while head != tail:
+        pr, tm = lst[head]
+        head = 0 if head+1 == n else head+1
         tm = int(tm)
         if tm <= q:
             time += tm
             ans.append((pr, time))
         else:
-            lst.append((pr, tm - q))
+            tail = 0 if tail+1 == n else tail+1
             time += q
+            lst[tail] = (pr, tm - q)
+    #最後の要素
+    pr, tm = lst[head]
+    ans.append((pr, time + int(tm)))
     return ans
 
 
@@ -19,6 +56,6 @@ for i in range(n):
     p, t = input().split()
     lst.append((p, t))
 ans = schedule(lst, q)
+#print("---")
 for pr, tm in ans:
-    print(pr,tm)
-
+    print(pr, tm)
